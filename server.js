4dddc01001fec6f13c4ee5c12dea68a692d44a65
Alexis Venegas <alexisvfilmstuff@protonmail.com>
2021-09-14 12:18:54 -0700

    fixed issues

--- server.js
+++ server.js
@@ -7,20 +7,22 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(require('./routes'))
 
-app.get('/notes', req, res) => {
+
+app.get('/notes', (req, res) => {
   res.sendFile('/public/notes.html', {root:__dirname})
   console.log('test')
 })
 
-app.get('/api/notes', req, res) => {
+app.get('/api/notes', (req, res) => {
   console.log('hi')
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'public / index.html'))
+  res.sendFile(join(__dirname, 'public/index.html'))
 })
 
+app.use(require('./routes'))
+
 app.listen(process.env.PORT || 3000)
-console.log(‘listening on port 3000’)
+console.log('listening on port 3000')
